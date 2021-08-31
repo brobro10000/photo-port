19a5dccfcde0430fb420516a631709e296fe0298
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-08-31 12:28:43 -0400

    Created nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 function Nav() {
     const categories = [
-        { name: "commercial", description:"Photos of grocery stores, food trucks, and other commercial projects",},
+        { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects", },
         { name: "portraits", description: "Portraits of people in my life" },
         { name: "food", description: "Delicious delicacies" },
-        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature"},
+        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature" },
     ];
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about">
                             About me
                         </a>
                     </li>
